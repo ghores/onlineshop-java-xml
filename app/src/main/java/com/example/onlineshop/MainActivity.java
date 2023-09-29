f696c1d15b0c9f8a84d564cdc2e935fce793d66e
Ghazimoradi <ghazimoradi.amin@gmail.com>
2023-09-29 15:59:28 +0330

    added drawer navigation/bottom navigation/appbar layout

--- app/src/main/java/com/example/onlineshop/MainActivity.java
+++ app/src/main/java/com/example/onlineshop/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.onlineshop;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mainDrawer;
+    private MaterialToolbar topAppBar;
+    private BottomNavigationView bottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        bindViews();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,
+                mainDrawer,
+                topAppBar,
+                R.string.open,
+                R.string.close
+        );
+        mainDrawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    private void bindViews() {
+        mainDrawer = findViewById(R.id.mainDrawer);
+        topAppBar = findViewById(R.id.topAppBAr);
+        bottomNav = findViewById(R.id.bottomNav);
     }
 }
