0f8a13d6bf4cc330a9539c637dc636a150f47ca1
Ghazimoradi <ghazimoradi.amin@gmail.com>
2023-09-29 17:18:15 +0330

    implementation Fragmentation

--- app/src/main/java/com/example/onlineshop/MainActivity.java
+++ app/src/main/java/com/example/onlineshop/MainActivity.java
@@ -1,19 +1,30 @@
 package com.example.onlineshop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
+import com.example.onlineshop.fragments.BasketFragment;
+import com.example.onlineshop.fragments.BlogFragment;
+import com.example.onlineshop.fragments.HomeFragment;
+import com.example.onlineshop.fragments.ProductFragment;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mainDrawer;
     private MaterialToolbar topAppBar;
     private BottomNavigationView bottomNav;
+    private FrameLayout mainFrame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,11 +44,50 @@ public class MainActivity extends AppCompatActivity {
         );
         mainDrawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        bottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+                /*switch (item.getItemId()) {
+                    case R.id.home:
+                        transaction.replace(R.id.mainFrame, new HomeFragment(MainActivity.this));
+                        break;
+                    case R.id.products:
+                        transaction.replace(R.id.mainFrame, new ProductFragment(MainActivity.this));
+                        break;
+                    case R.id.blog:
+                        transaction.replace(R.id.mainFrame, new BlogFragment(MainActivity.this));
+                        break;
+                    case R.id.basket:
+                        transaction.replace(R.id.mainFrame, new BasketFragment(MainActivity.this));
+                        break;
+                }*/
+                int itemId = item.getItemId();
+                if (itemId == R.id.home) {
+                    transaction.replace(R.id.mainFrame, new HomeFragment(MainActivity.this));
+                } else if (itemId == R.id.products) {
+                    transaction.replace(R.id.mainFrame, new ProductFragment(MainActivity.this));
+                } else if (itemId == R.id.blog) {
+                    transaction.replace(R.id.mainFrame, new BlogFragment(MainActivity.this));
+                } else if (itemId == R.id.basket) {
+                    transaction.replace(R.id.mainFrame, new BasketFragment(MainActivity.this));
+                }
+
+                //transaction.addToBackStack(null);
+                transaction.commit();
+                return true;
+            }
+        });
+        bottomNav.setSelectedItemId(R.id.home);
     }
 
     private void bindViews() {
         mainDrawer = findViewById(R.id.mainDrawer);
         topAppBar = findViewById(R.id.topAppBAr);
         bottomNav = findViewById(R.id.bottomNav);
+        mainFrame = findViewById(R.id.mainFrame);
     }
 }
---
+++ app/src/main/java/com/example/onlineshop/fragments/BasketFragment.java
@@ -0,0 +1,29 @@
+package com.example.onlineshop.fragments;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.onlineshop.R;
+
+public class BasketFragment extends Fragment {
+
+    private Activity activity;
+
+    public BasketFragment(Activity activity) {
+        this.activity = activity;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.frame_basket, container, false);
+        return rootView;
+    }
+}
