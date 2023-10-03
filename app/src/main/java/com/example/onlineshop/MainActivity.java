a37089f332eb8d2b91c7257dba18f17df2fd3694
Ghazimoradi <ghazimoradi.amin@gmail.com>
2023-10-03 11:00:07 +0330

    implementation onBack press

--- app/src/main/java/com/example/onlineshop/MainActivity.java
+++ app/src/main/java/com/example/onlineshop/MainActivity.java
@@ -8,16 +8,22 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import com.example.onlineshop.fragments.BasketFragment;
 import com.example.onlineshop.fragments.BlogFragment;
 import com.example.onlineshop.fragments.HomeFragment;
+import com.example.onlineshop.fragments.LoginFragment;
 import com.example.onlineshop.fragments.ProductFragment;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
     private MaterialToolbar topAppBar;
     private BottomNavigationView bottomNav;
     private FrameLayout mainFrame;
+    private NavigationView navigationView;
+    private boolean doubleBackToExitPressedOnce = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,6 +90,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         bottomNav.setSelectedItemId(R.id.home);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                @SuppressLint("CommitTransaction") FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+                int itemId = item.getItemId();
+                if (itemId == R.id.login) {
+                    transaction.replace(R.id.mainFrame, new LoginFragment(MainActivity.this));
+                } else if (itemId == R.id.register) {
+                   // transaction.replace(R.id.mainFrame, new RegisterFragment(MainActivity.this));
+                } else if (itemId == R.id.about) {
+                    //transaction.replace(R.id.mainFrame, new AboutFragment(MainActivity.this));
+                }
+                transaction.commit();
+                mainDrawer.closeDrawer(Gravity.LEFT);
+                return true;
+            }
+        });
     }
 
     private void bindViews() {
@@ -89,5 +116,25 @@ public class MainActivity extends AppCompatActivity {
         topAppBar = findViewById(R.id.topAppBAr);
         bottomNav = findViewById(R.id.bottomNav);
         mainFrame = findViewById(R.id.mainFrame);
+        navigationView = findViewById(R.id.navigationView);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+        this.doubleBackToExitPressedOnce = true;
+        Snackbar snackbar = Snackbar.make(mainFrame, R.string.twice_back, Snackbar.LENGTH_LONG);
+        snackbar.getView().setTranslationY(-135);
+        snackbar.show();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce = false;
+            }
+        }, 2000);
     }
 }
---
+++ app/src/main/java/com/example/onlineshop/fragments/LoginFragment.java
@@ -0,0 +1,29 @@
+package com.example.onlineshop.fragments;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.onlineshop.R;
+
+public class LoginFragment extends Fragment {
+
+    private Activity activity;
+
+    public LoginFragment(Activity activity) {
+        this.activity = activity;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.frame_login, container, false);
+        return rootView;
+    }
+}
