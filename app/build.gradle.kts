0ce13fabb1b81b0f10c3c8c38163a1f10f46edc2
Ghazimoradi <ghazimoradi.amin@gmail.com>
2024-12-27 13:39:41 +0330

    fix ApiAddress and increase library version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 
 android {
     namespace = "com.example.onlineshop"
-    compileSdk = 34
+    compileSdk = 35
     defaultConfig {
         applicationId = "com.example.onlineshop"
-        minSdk = 23
-        targetSdk = 34
+        minSdk = 22
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -32,13 +32,13 @@ android {
 
 dependencies {
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.activity:activity:1.8.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("androidx.activity:activity:1.9.3")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
     //Slide Show
     implementation("com.github.smarteist:autoimageslider:1.3.9")
